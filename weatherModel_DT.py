3f3da4736b33a712d2e5ce4fc9e3251d67488772
Mano Ranjitham <mmanoranjitham7@gmail.com>
2025-11-18 00:18:22 +0530

    Files are uploded successfully

---
+++ weatherModel_DT.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load model & encoders
+model = joblib.load("weather_pipeline_model.pkl")
+encoders = joblib.load("weather_label_encoders.pkl")
+
+@app.route("/", methods=["GET"])
+def home():
+    return {"message": "Weather Prediction API is Running!"}
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.json
+
+    input_df = pd.DataFrame([data])
+
+    # Encode input using saved encoders
+    for col in input_df.columns:
+        if col in encoders:
+            input_df[col] = encoders[col].transform(input_df[col])
+
+    # Predict using pipeline
+    prediction = model.predict(input_df)[0]
+
+    return jsonify({
+        "input_data": data,
+        "prediction": int(prediction)
+    })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
