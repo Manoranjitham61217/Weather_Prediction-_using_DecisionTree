85a3d81b780a39ac6d00a127519278fa1a5b52ab
Mano Ranjitham <mmanoranjitham7@gmail.com>
2025-11-18 07:44:21 +0530

    Added HTML UI and updated predict route

--- weatherModel_DT.py
+++ weatherModel_DT.py
@@ -1,41 +1,43 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import joblib
 import pandas as pd
 
 app = Flask(__name__)
 model = joblib.load("weather_pipeline_model.pkl")
 encoders = joblib.load("weather_label_encoders.pkl")
+@app.route('/home')
+def home():
+    return render_template("index.html")
+
 
 
 @app.route("/", methods=["GET"])
-def home():
-    return jsonify({"message": "Weather Prediction API is Running!"})
-
-
-@app.route("/predict", methods=["POST"])
+@app.route('/predict', methods=['POST'])
 def predict():
     try:
         data = request.get_json()
+        temperature = float(data['temperature'])
+        humidity = float(data['humidity'])
+        precipitation = float(data['precipitation'])
+        cloud_cover = float(data['cloud_cover'])
+        uv_index = float(data['uv_index'])
+        season = float(data['season'])
+        visibility = float(data['visibility'])
+ 
+        final_input = [[
+            temperature,
+            humidity,
+            precipitation,
+            cloud_cover,
+            uv_index,
+            season,
+            visibility
+        ]]
 
-        if data is None:
-            return jsonify({"error": "Invalid input. Please send JSON data."}), 400
+        prediction = model.predict(final_input)[0]
 
-        input_df = pd.DataFrame([data])
-        for col in input_df.columns:
-            if col in encoders:
-                try:
-                    input_df[col] = encoders[col].transform(input_df[col])
-                except Exception as e:
-                    return jsonify({
-                        "error": f"Value '{input_df[col].iloc[0]}' is not valid for column '{col}'.",
-                        "valid_values": list(encoders[col].classes_)
-                    }), 400
-        prediction = model.predict(input_df)[0]
+        return jsonify({"prediction": str(prediction)})
 
-        return jsonify({
-            "input_data": data,
-            "prediction": int(prediction)
-        })
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
